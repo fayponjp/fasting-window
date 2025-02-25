512dc208d6f7d58368c1b8470f26fa599bb5aacd
fayponjp <fayponjp@gmail.com>
2025-02-25 17:39:20 +0800

    Hour fast updates - UI and functionality, updated logo

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const progressOutlineElement = document.getElementById("progressBorder");
 const timerDisplay = document.getElementById("timerDisplay");
 const finishButton = document.getElementById("finishButton");
+const updateButton = document.getElementById("updateButton");
 const cancelButton = document.getElementById("cancelButton");
-const activeFastElementsArray = [progressOutlineElement, timerDisplay, finishButton, cancelButton];
+const activeFastElementsArray = [progressOutlineElement, timerDisplay, finishButton, updateButton, cancelButton];
 
 const startButton = document.getElementById("startButton");
 const hourOptions = document.querySelector(".hourOptions");
@@ -11,19 +12,18 @@ const inactiveFastElementsArray = [startButton, hourOptions];
 const fastHourButtons = document.querySelectorAll(".timeButton");
 
 let fastHours = 16;
+let loopVariable = "";
 
-startButton.addEventListener("click", clickStartHandler);
-
-for (let i = 0; i < fastHourButtons.length; i++) {
-    fastHourButtons[i].addEventListener("click", () => {
-        selectFastHoursHandler(fastHourButtons[i]);
-    });
+function updateFastTimeText(time) {
+    const fastTimeText = document.getElementById("fastTime");
+    fastTimeText.textContent = time;
 }
 
 function getFastEndTime(selectedFastHours, startTime) {
     let dateTimeValue = new Date(startTime);
-    dateTimeValue.setTime(dateTimeValue.getTime() + selectedFastHours*60*60*1000);
-    console.log(dateTimeValue);
+    dateTimeValue.setTime(startTime.getTime() + selectedFastHours*60*60*1000);
+
+    return dateTimeValue;
 }
 
 function getDateTime() {
@@ -45,22 +45,45 @@ function getDateTime() {
     return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
 }
 
-function updateTimerDisplay() {
+function updateTimerDisplay(startTime, endTime) {
+    const goalSeconds = endTime.getTime() - startTime.getTime();
+    console.log(goalSeconds/1000/3600);
+
     const timeDisplayElement = document.getElementById("timerDisplay");
-    const dateTimeValue = getDateTime();
+    const progressBarElement = document.getElementById("progressBar");
 
-    timeDisplayElement.textContent =`Time Started: ${dateTimeValue}`;
+    // timeDisplayElement.textContent =`Time Started: ${dateTimeValue}`;
 
-    // setInterval("updateTimerDisplay()", 1000);
+    loopVariable = setInterval(() => {
+        const currentTime = new Date();
+        const secondsSinceStart = currentTime.getTime() - startTime.getTime();
+        const differenceInTimeRatio = Math.round((secondsSinceStart / goalSeconds) * 100);
+
+        progressBarElement.style.width = `${differenceInTimeRatio}%`;
+        progressBarElement.style.transition = 'width 2s';
+
+        if (differenceInTimeRatio > 100) {
+            progressBarElement.style.backgroundColor = '#8cb369';
+            progressOutlineElement.style.border = '1px solid #8cb369';
+        }
+
+        console.log(differenceInTimeRatio);
+    }, 1000);
 }
 
 function clickStartHandler() {
-    const startDateTime = getDateTime();
-    // startDateTime = '2025-2-23 12:50:30';
-    localStorage.setItem("startTime", startDateTime);
+    // const startDateTime = getDateTime();
+    let startDateTime = new Date();
+    startDateTime = new Date('February 24, 2025 11:15:00');
+    // localStorage.setItem("startTime", startDateTime);
     toggleElementDisplay(inactiveFastElementsArray, activeFastElementsArray);
-    getFastEndTime(fastHours, startDateTime);
-    // updateTimerDisplay();
+    const endDateTime = getFastEndTime(fastHours, startDateTime);
+
+    updateTimerDisplay(startDateTime, endDateTime);
+}
+
+function cancelFastHandler() {
+    clearInterval(loopVariable);
 }
 
 function selectFastHoursHandler(fastHourElement) {
@@ -69,11 +92,11 @@ function selectFastHoursHandler(fastHourElement) {
         if (currentSelectedElement) currentSelectedElement.classList.remove("selected");
         fastHourElement.classList.add("selected");
         fastHours = parseInt(fastHourElement.textContent);
+        updateFastTimeText(fastHours);
     }
 }
 
 function toggleElementDisplay(arrayToHide, arrayToDisplay) {
-    console.log(arrayToHide);
     for (let i = 0; i < arrayToHide.length; i++) {
         arrayToHide[i].classList.add("hidden");
     }
@@ -83,6 +106,14 @@ function toggleElementDisplay(arrayToHide, arrayToDisplay) {
     }
 }
 
-function toggleNoFastElements() {
 
+startButton.addEventListener("click", clickStartHandler);
+cancelButton.addEventListener("click", cancelFastHandler);
+
+for (let i = 0; i < fastHourButtons.length; i++) {
+    fastHourButtons[i].addEventListener("click", () => {
+        selectFastHoursHandler(fastHourButtons[i]);
+    });
 }
+
+updateFastTimeText(fastHours);
